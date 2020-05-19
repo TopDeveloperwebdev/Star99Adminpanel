af77d05b55f0e6603e02309370629d09781b3d52
vladimirpotanin <vladimir971001@yandex.ru>
2020-05-19 09:18:49 +0200

    'uploadlogo'

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -1,7 +1,8 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4" style="min-height: 917px;">
     <!-- Brand Logo -->
     <a href="#" class="brand-link">
-        <span class="brand-text font-weight-light">{{ trans('panel.site_title') }}</span>
+        <span class="brand-text font-weight-light"><img src="/img/logo.png" style="width: 93px;
+    margin-left: 25%;" /></span>
     </a>
 
     <!-- Sidebar -->
@@ -22,12 +23,11 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="{{ route("admin.transactions.index") }}" class="nav-link {{ request()->is('admin/transactions') || request()->is('admin/transactions/*') ? 'active' : '' }}">
-                        <i class="fa-fw fas fa-credit-card">
-
+                    <a class="nav-link {{ request()->is('admin/users') || request()->is('admin/users/*') ? 'active' : '' }}" href="{{ route("admin.users.index") }}">
+                        <i class="fa-fw fas fa-users">
                         </i>
                         <p>
-                            <span>Play Management</span>
+                            <span>User Management</span>
                         </p>
                     </a>
                 </li>
@@ -41,16 +41,17 @@
                         </p>
                     </a>
                 </li>
-
                 <li class="nav-item">
-                    <a class="nav-link {{ request()->is('admin/users') || request()->is('admin/users/*') ? 'active' : '' }}" href="{{ route("admin.users.index") }}">
-                        <i class="fa-fw fas fa-users">
+                    <a href="{{ route("admin.transactions.index") }}" class="nav-link {{ request()->is('admin/transactions') || request()->is('admin/transactions/*') ? 'active' : '' }}">
+                        <i class="fa-fw fas fa-credit-card">
+
                         </i>
                         <p>
-                            <span>User Management</span>
+                            <span>Play Management</span>
                         </p>
                     </a>
                 </li>
+
                 <li class="nav-item">
                     <a href="#" class="nav-link" onclick="event.preventDefault(); document.getElementById('logoutform').submit();">
                         <p>
